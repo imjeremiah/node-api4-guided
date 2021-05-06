ecb4958080b845add1aefa8da8dfd164335c0a77
Jeremiah Candelaria <jeremiahcandelaria@gmail.com>
2021-05-06 10:56:13 -0700

    wip

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
-
+const path = require('path');
 const express = require('express');
 
 const server = express();
 
 const PORT = process.env.PORT || 8000;
+
+server.use(express.static(path.join(__dirname, 'client/build')));
 
 server.use(express.json());
 
@@ -12,6 +14,10 @@ server.get('/api/*', (req, res) => {
     res.json({
         cohort: 'Web 41'
     });
+});
+
+server.use('*', (_, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 });
 
 server.listen(PORT, () => {
