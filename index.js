241ec5343d7d3a24dee09b728e1131c6d68eb843
Jeremiah Candelaria <jeremiahcandelaria@gmail.com>
2021-05-06 11:07:43 -0700

    wip

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-require('dotenv').config();
-const path = require('path');
-const express = require('express');
-
-const server = express();
-
-const PORT = process.env.PORT || 8000;
-
-server.use(express.static(path.join(__dirname, 'client/build')));
-
-server.use(express.json());
-
+require('dotenv').config()
+const path = require('path')
+const express = require('express')
+const server = express()
+const PORT = process.env.PORT || 8000
+process.env.API_KEY || 'gaga'
+server.use(express.static(path.join(__dirname, 'client/build')))
+server.use(express.json())
 server.get('/api/*', (req, res) => {
-    res.json({
-        cohort: 'Web 41'
-    });
-});
-
-server.use('*', (_, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-});
-
+  res.json({
+    cohort: 'Web 41',
+  })
+})
+server.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
 server.listen(PORT, () => {
-    console.log(`listening on ${PORT}`);
-});
+  console.log(`listening on ${PORT}`)
+})
